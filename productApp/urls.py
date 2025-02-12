c56ae60a2423b521bc4b15d5e0b02c2b8ae178e4
Arise Damilare <arisedamilare5@gmail.com>
2025-02-12 08:37:47 +0100

    Refactor product API views to use GenericAPIView for improved structure and maintainability

--- productApp/urls.py
+++ productApp/urls.py
@@ -1,8 +1,29 @@
-from django.urls import path
-from .views import ProductView
+from django.urls import path, include
+# from .views import ProductView
+# from .views import ProductViewSet
+# from .views import createProduct
+from .views import productView
+from rest_framework.routers import DefaultRouter
+
+# urlpatterns = [
+#     # path('product/<int:pk>/', ProductView.as_view()),
+#     path('product/', ProductView.as_view()),
+# ]
+ 
+ 
+# using viewSet
+
+# router = DefaultRouter()
+# router.register('product', ProductViewSet, basename='product')
+# urlpatterns = [
+#     path('', include(router.urls)),
+# ]
+
+
+
+# using genericApiView
 
 urlpatterns = [
-    path('product/<int:pk>/', ProductView.as_view()),
-    path('product/', ProductView.as_view()),
+    # path('product/', createProduct.as_view()),
+    path('product/', productView.as_view()),
 ]
- 
--- productApp/views.py
+++ productApp/views.py
@@ -3,53 +3,90 @@ from rest_framework.views import APIView
 from .models import Product
 from .serializers import ProductSerializer
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, viewsets
+from rest_framework.generics import GenericAPIView, CreateAPIView
+
 # Create your views here.
 
 # APIView
 # GenericVIew
 # viewSet
 
-class ProductView(APIView):
+# class ProductView(APIView):
     
-    def get(self, request, pk=None):
-        if pk:
-            product = get_object_or_404(Product, pk=pk)
-            many = False
+#     def get(self, request, pk=None):
+#         if pk:
+#             product = get_object_or_404(Product, pk=pk)
+#             many = False
             
-        else:
+#         else:
             
-            product = Product.objects.all()
-            many = True
+#             product = Product.objects.all()
+#             many = True
             
             
-        serializer = ProductSerializer(product, many=many)
+#         serializer = ProductSerializer(product, many=many)
+#         return Response(serializer.data, status=status.HTTP_200_OK)
+    
+#     def post(self, request, pk=None):
+#         serializer = ProductSerializer(data = request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+            
+#             return Response({"message": "Product added successfully", "data": serializer.data}, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+#     def put(self, request, pk):
+#         try:
+#             product = Product.objects.get(pk=pk)
+#         except Product.DoesNotExist:
+#             return Response({"message": "Product not found"}, status=status.HTTP_404_NOT_FOUND)
+        
+#         serializer = ProductSerializer(instance=product, data=request.data, partial=True)
+#         if serializer.is_valid():
+#             serializer.save()
+            
+#             return Response({"message":'Product updated successfully', "data":serializer.data}, status=status.HTTP_200_OK)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+#     def delete(self, request, pk):
+#         product = get_object_or_404(Product, pk=pk)
+#         product.delete()
+        
+#         return Response({"message":"Product deleted successfully"}, status=status.HTTP_200_OK)
+        
+        
+# using viewSet
+# class ProductViewSet(viewsets.ModelViewSet):
+#     queryset = Product.objects.all()
+#     serializer_class = ProductSerializer
+#     # permission_classes = [IsAuthenticated, IsAdminUser]
+    
+
+# using GenericAPIView
+   
+class productView(GenericAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    # permission_classes = [IsAuthenticated, IsAdminUser]
+
+    def get(self, request):
+        products = Product.objects.all()
+        serializer = self.get_serializer(products, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
     
-    def post(self, request, pk=None):
-        serializer = ProductSerializer(data = request.data)
-        if serializer.is_valid():
-            serializer.save()
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(
+            {"message": "Product added successfully", "data": serializer.data}, status=status.HTTP_201_CREATED
+        )
             
-            return Response({"message": "Product added successfully", "data": serializer.data}, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-    def put(self, request, pk):
-        try:
-            product = Product.objects.get(pk=pk)
-        except Product.DoesNotExist:
-            return Response({"message": "Product not found"}, status=status.HTTP_404_NOT_FOUND)
         
-        serializer = ProductSerializer(instance=product, data=request.data, partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            
-            return Response({"message":'Product updated successfully', "data":serializer.data}, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-    def delete(self, request, pk):
-        product = get_object_or_404(Product, pk=pk)
-        product.delete()
-        
-        return Response({"message":"Product deleted successfully"}, status=status.HTTP_200_OK)
-        
+
+
+# class createProduct(CreateAPIView):
+#     queryset = Product.objects.all()
+#     serializer_class = ProductSerializer
+    # permission_classes = [IsAuthenticated, IsAdminUser]
